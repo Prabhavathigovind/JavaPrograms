1fca8c28f4f818be158f41fd0ddb3af53b8c2a68
Prabha <govindprabs@gmail.com>
2025-03-06 18:01:09 +0530

    todayscomit

--- src/JavaPrograms/BasicPrograms.java
+++ src/JavaPrograms/BasicPrograms.java
@@ -1,10 +1,20 @@
 package JavaPrograms;
 
+import java.util.Scanner;
+
 public class BasicPrograms {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+System.out.println("hello");
+Scanner scan = new Scanner(System.in);
+System.out.println("enter name");
+scan.next();    
 
+System.out.println("enter number");
+scan.nextInt();
+		
 	}
 
 }
